502882f76e0043507ed1cb0417a756e8470132bd
TahaNamdar <tahanamdar30@gmail.com>
2026-05-13 12:08:20 +0330

    fix tailwind config err

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,6 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-  ],
   theme: {
     extend: {
       colors: {
